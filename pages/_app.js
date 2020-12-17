95b08a9c224cc38f314c47429f204029fb8f605e
Simon Vrachliotis <simon@simonswiss.com>
2020-12-17 16:44:05 +1100

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+import "typeface-roboto";
+
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
